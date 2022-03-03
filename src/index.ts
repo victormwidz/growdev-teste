7fb3b9250250645a28499c93e8c0d56512cbe7ec
victor <victormwidz@gmail.com>
2022-03-03 14:23:52 -0300

    criado rota padrão e readme

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
-import express, {Request, Response}from 'express';
+import express, {Request, response, Response}from 'express';
 import cors from 'cors';
+import { request } from 'http';
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use (cors ());
+
+app.get('/', (request: Request, response: Response) => {
+    return response.send('ok');
+});
 
 app.listen (8080, ()=> {
     console.log('api rodando....');
